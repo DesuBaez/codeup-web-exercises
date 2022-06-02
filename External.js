4c68cfcc20d1adedd390a7db8742a724ab4aedd1
David Baez <david.des.baez22@gmail.com>
2022-06-01 22:28:33 -0500

    almost new stuff

---
+++ External.js
@@ -0,0 +1,11 @@
+
+var mermaid = 3*3;
+var bear  = 3*5;
+var hercules = 1*3;
+var cost = mermaid + bear + hercules;
+prompt ("what will that cost, friendly blockbuster person?")
+if (input == 27);{
+
+}
+
+else alert ("don't be a cheap azz!");
