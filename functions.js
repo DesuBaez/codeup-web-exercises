a0067e6796a62c70695d3b239a6fc85502a063a0
David Baez <david.des.baez22@gmail.com>
2022-06-03 13:27:52 -0500

    slings%arrows

---
+++ functions.js
@@ -0,0 +1,127 @@
+"use strict";
+alert("Yo lets make sure this works")
+/**
+ * TODO:
+ * Create a function called 'sayHello' that takes a parameter 'name'.
+ * When called, the function should return a message that says hello to the passed in name.
+ *
+ * Example
+ * > sayHello("codeup") // returns "Hello, codeup!"
+ */
+function sayHello (Input) {
+  return ("Hello "+ Input)
+}
+ console.log (sayHello("David"))
+
+/**
+ * TODO:
+ * Call the function 'sayHello' and pass your name as a string literal argument.
+ * Store the result of the function call in a variable named 'helloMessage'.
+ *
+ * console.log 'helloMessage' to check your work
+ */
+ sayHello ("David")
+let helloMessage = sayHello
+console.log ("helloMessage")
+
+/**
+ * TODO:
+ * Store your name as a string in a variable named 'myName', and pass that
+ * variable to the 'sayHello' function. You should see the same output in the
+ * console.
+ */
+let myName = ("David")
+sayHello(myName)
+console.log (sayHello(myName))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var random = Math.floor((Math.random() * 3) + 1);
+
+/**
+ * TODO:
+ * Create a function called 'isTwo' that takes a number as a parameter.
+ * The function should return a boolean value based on whether or not the passed
+ * number is the number 2.
+ *
+ *
+ * Example
+ * > isTwo(1) // returns false
+ * > isTwo(2) // returns true
+ * > isTwo(3) // returns false
+ *
+ * Call the function 'isTwo' passing the variable 'random' as a argument.
+ *
+ * console.log *outside of the function* to check your work (you should see a
+ * different result everytime you refresh the page if you are using the random
+ * number)
+ */
+
+function isTwo (x) {
+    return  num === 2
+}
+
+
+
+
+/**
+ * TODO:
+ * Create a function named 'calculateTip' to calculate a tip on a bill at a
+ * restaurant. The function should accept a tip percentage and the total of the
+ * bill, and return the amount to tip
+ *
+ * Examples:
+ * > calculateTip(0.20, 20) // returns 4
+ * > calculateTip(0.25, 25.50) // returns 6.375
+ * > calculateTip(0.15, 33.42) // returns 5.013
+ */
+ function calculateTip (tipAmount , billTotal){
+
+     let tipInDollars = tipAmount * billTotal;
+     return tipInDollars;
+ }
+
+/**
+ * TODO:
+ * Use prompt and alert in combination with your calculateTip function to
+ * prompt the user for the bill total and a percentage they would like to tip,
+ * then display the dollar amount they should tip
+ */
+ let totalAmount = prompt ("enter bill total")
+ let tipAsDecimal = prompt ("enter tip amount" )
+ alert (tipTotal)
+
+/**
+ * TODO:
+ * Create a function named `applyDiscount`. This function should accept a price
+ * (before a discount is applied), and a discount percentage (a number between 0
+ * and 1). It should return the result of applying the discount to the original
+ * price.
+ *
+ * > var originalPrice = 100;
+ * > var discountPercent = .2; // 20%
+ * > applyDiscount(originalPrice, discountPercent) // 80
+ *
+ * > applyDiscount(45.99, 0.12) // 40.4712
+ *
+ *
+ */
+function applyDiscount (price, discountAsDecimal){
+
+
+    return originalPrice - (originalPrice * discountAsDecimal)
+
+
+ }
---
+++ scratch_js.js
@@ -0,0 +1,27 @@
+alert ("Begin the race")
+
+function  analyzeColor (input) {
+
+
+    if (input === "blue"){
+        Console.log("blue is the color of my eyes")
+
+    }
+
+else
+    if (input === red){
+        Console.log("red,, like the color you bleed")
+
+    }
+
+
+else
+    if (input === black){
+        Console.log("black like the color of your heart")
+
+    }
+
+else
+    return message
+    ( " I do not have that color")
+}
