d967d219785842af3370d7139b38cc70233c3390
David Baez <david.des.baez22@gmail.com>
2022-06-16 10:21:14 -0500

    welcome first style

--- scratch_js.js
+++ scratch_js.js
@@ -14,8 +14,25 @@
 
 
 
+const companies =[
+    {name:"company one ", category:"finance",start:1981,end:2003},
+    {name:"company two", category:"retail",start:1981,end:2003},
+    {name:"company three", category:"auto",start:1981,end:2003},
+    {name:"company four", category:"retail",start:1981,end:2003},
+    {name:"company five", category:"technology",start:1981,end:2003},
+    {name:"company six", category:"finance",start:1981,end:2003},
+    {name:"company seven", category:"auto",start:1981,end:2003},
+    {name:"company eight", category:"finance",start:1981,end:2003},
+    {name:"company nine", category:"retail",start:1981,end:2003},
+];
 
+const age= [33,12,20,16,5,54,21,44,61,13,15,45,25,64,32];
 
+// forEach
+// filter
+// map
+// sort
+// reduce
 
 
 
